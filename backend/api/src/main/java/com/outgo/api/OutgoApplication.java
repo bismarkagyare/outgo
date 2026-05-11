2e398be287c990cc27e0f7fbc10d257a8e61fb52
Bismark Agyare <bismarkagyare54@gmail.com>
2026-05-11 15:21:28 +0000

    scaffold backend

---
+++ backend/api/src/main/java/com/outgo/api/OutgoApplication.java
@@ -0,0 +1,13 @@
+package com.outgo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutgoApplication.class, args);
+	}
+
+}
